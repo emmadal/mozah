9ae8b0b405ac56290674775c75f1bdcd512ca830
emmadal <emmanueldalougou@gmail.com>
2022-09-19 21:25:38 +0000

    add subscription description

---
+++ src/data/index.ts
@@ -0,0 +1,42 @@
+export const subscription = [
+  {
+    id: '1',
+    title: 'Offre Standard',
+    token: '100 Tokens offerts',
+    investment: '20% de retour sur investissement',
+    productivity: '100% de Rendement totaux',
+    value: '20% de la valeur du Token en USDT',
+    price: '100 - 750 €',
+  },
+  {
+    id: '2',
+    title: 'Offre Premium',
+    token: '300 Tokens offerts',
+    investment: '25% de retour sur investissement',
+    productivity: '125% de Rendement totaux',
+    value: '25% de la valeur du Token en USDT',
+    price: '800 - 2500 €',
+  },
+  {
+    id: '3',
+    title: 'Offre Gold',
+    token: '500 Tokens offerts',
+    investment: '30% de retour sur investissement',
+    productivity: '150% de Rendement totaux',
+    value: '30% de la valeur du Token en USDT',
+    price: '2750 - 10000 €',
+  },
+];
+
+export const payments = [
+  {
+    id: 1,
+    name: 'Paypal',
+    img: require('../assets/images/paypal.png'),
+  },
+  {
+    id: 2,
+    name: 'Orange Money',
+    img: require('../assets/images/orangemoney.png'),
+  },
+];
